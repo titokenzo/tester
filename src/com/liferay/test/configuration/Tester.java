b94b6bfa331929e36a5e836458fbe8e422f7e689
Tito Kenzo <titokenzo@gmail.com>
2022-01-04 14:52:56 -0300

    TEST-4 SF

--- src/com/liferay/test/configuration/Tester.java
+++ src/com/liferay/test/configuration/Tester.java
@@ -5,15 +5,10 @@ import com.germinus.easyconf.ComponentProperties;
 import com.germinus.easyconf.EasyConf;
 
 public class Tester {
-
 	public static void main(String[] args) {
-
 		ComponentConfiguration conf = EasyConf.getConfiguration("tester");
 		ComponentProperties props = conf.getProperties();
-
 		String name = props.getString("tester.name");
-
 		System.out.println("Tester name: " + name);
 	}
-
 }
